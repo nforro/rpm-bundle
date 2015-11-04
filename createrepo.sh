53487c268310f7a8ab1ca0a7a45b4bb1a051922f
Nikola Forró <nforro@redhat.com>
2015-11-04 16:54:50 +0100

    Relative paths in createrepo script, regenerated repo content

--- createrepo.sh
+++ createrepo.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-target_dir="$(pwd)/repo"
 gpg_key_id="133F16C2"
 
-apt-ftparchive packages "$target_dir" > "$target_dir/Packages" || exit 1
-gzip --to-stdout -9 "$target_dir/Packages" > "$target_dir/Packages.gz" || exit 1
+startdir="$(pwd)"
+workdir="$startdir/repo"
 
-apt-ftparchive -o 'Release::Codename="precise"' \
-               -o 'Release::Components="main"' \
-               -o 'Release::Architectures="i386 amd64"' \
-               release "$target_dir" > "$target_dir/Release" || exit 1
+trap "{ cd \"$startdir\"; }" EXIT
 
-rm --force "$target_dir/Release.gpg"
+cd "$workdir"
+
+apt-ftparchive packages . > Packages || exit 1
+gzip --to-stdout -9 Packages > Packages.gz || exit 1
+
+apt-ftparchive -o 'APT::FTPArchive::Release::Codename="precise"' \
+               -o 'APT::FTPArchive::Release::Components="main"' \
+               -o 'APT::FTPArchive::Release::Architectures="i386 amd64"' \
+               release . > Release || exit 1
+
+rm --force Release.gpg
 gpg --armor --detach-sign --default-key $gpg_key_id \
-    --output "$target_dir/Release.gpg" --sign "$target_dir/Release" || exit 1
+    --output Release.gpg --sign Release || exit 1
