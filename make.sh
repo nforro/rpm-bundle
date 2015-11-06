16fd39a6daed5c7cb82f4c99834d5d542cfc9685
Nikola Forró <nforro@redhat.com>
2015-11-06 13:51:26 +0100

    Better structure and file names

--- make.sh
+++ make.sh
@@ -4,16 +4,19 @@ workdir="$(pwd)"
 srcdir="/mnt/src"
 repodir="$workdir/repo"
 
-image="local:ubuntu_precise_deps"
+image="nforro/ubuntu-precise-dev:latest"
 
 gpg_key_id="133F16C2"
 
 # enable docker access to files in working dir
-chcon -Rt svirt_sandbox_file_t "$workdir"
+chcon -Rt svirt_sandbox_file_t "$workdir" || exit 1
+
+# pull docker image
+sudo docker pull "$image" || exit 1
 
 # run docker image in temporary container
 sudo docker run --rm -v "$workdir:$srcdir" "$image" \
-    /bin/sh "$srcdir/build.sh" || exit 1
+    /bin/sh "$srcdir/docker/build.sh" || exit 1
 
 # reset ownership
 sudo chown "$(id -u):$(id -g)" "$repodir/"*
